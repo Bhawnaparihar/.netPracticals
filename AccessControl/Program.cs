b2bed87ef20be314304b18e53e2c7fc0eaccfabb
Bhawna Parihar <52083996+Bhawnaparihar@users.noreply.github.com>
2020-05-03 16:11:23 +0530

    Add files via upload

---
+++ AccessControl/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace AccessControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "TestFile.txt";
+            using (StreamWriter writer =
+                File.CreateText(fileName))
+            {
+                writer.WriteLine(
+                "Lorem ipsum dolor sit amet...");
+            }
+
+            var stream = File.Open(
+                   fileName, FileMode.Open);
+            var fileSecurity =
+                   stream.GetAccessControl();
+
+            foreach (var item in
+                fileSecurity.GetAccessRules(
+                true, true, typeof(NTAccount)))
+            {
+                var rule = item as
+                  FileSystemAccessRule;
+                Console.WriteLine(
+                  "Identity Reference : {0}",
+                  rule.IdentityReference.Value);
+                Console.WriteLine(
+                  "Access Control Type: {0}",
+                  rule.AccessControlType);
+                Console.WriteLine(
+                  "File System Rights : {0}\n",
+                  rule.FileSystemRights);
+            }
+        }
+    }
+}
---
+++ VerifyRoleMembership/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Principal;
+using System.Threading;
+
+
+namespace VerifyRoleMembership
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.SetPrincipalPolicy(
+                PrincipalPolicy.WindowsPrincipal);
+            WindowsPrincipal principal =
+                (WindowsPrincipal)Thread.CurrentPrincipal;
+            bool isAdmin = principal.IsInRole(
+                WindowsBuiltInRole.Administrator);
+
+            Console.WriteLine(
+                "You are {0}in the Administrators role",
+                isAdmin ? "" : "not ");
+        }
+
+    }
+}
